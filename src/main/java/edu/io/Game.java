058a105287de0980b4378063304a7ce0329e0627
Kamil Przęczek <dinokamil637@gmail.com>
2025-10-25 15:39:56 +0200

    tydzien 3 final

--- src/main/java/edu/io/Game.java
+++ src/main/java/edu/io/Game.java
@@ -2,6 +2,8 @@ package edu.io;
 
 import edu.io.token.PlayerToken;
 
+import java.util.Scanner;
+
 public class Game {
     private final Board board;
     private Player player;
@@ -14,7 +16,36 @@ public class Game {
         PlayerToken playerToken = new PlayerToken(player, board);
         player.assignToken(playerToken);
     }
-    public void start(){}
+    public void start(){
+        Scanner input = new Scanner(System.in);
+        boolean flag = true;
+
+        Player player1 = new Player();
+        this.join(player1);
+
+        while(flag){
+            System.out.println("Which direction do you want to move? (up, down, left, right or none)");
+            switch((input.nextLine()).toUpperCase()){
+                case "DOWN":
+                    player1.token().move(PlayerToken.Move.DOWN);
+                    break;
+                case "UP":
+                    player1.token().move(PlayerToken.Move.UP);
+                    break;
+                case "RIGHT":
+                    player1.token().move(PlayerToken.Move.RIGHT);
+                    break;
+                case "LEFT":
+                    player1.token().move(PlayerToken.Move.LEFT);
+                    break;
+                case "BREAK":
+                    flag = false;
+                    break;
+                default:
+                    player1.token().move(PlayerToken.Move.NONE);
+            }
+        }
+    }
 
     public Player player(){
         return player;
--- src/main/java/edu/io/Main.java
+++ src/main/java/edu/io/Main.java
@@ -5,33 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        boolean flag = true;
-        Board board = new Board();
-        Player player = new Player();
-        PlayerToken playerToken = new PlayerToken(player, board);
-
-        while(flag){
-            System.out.println("Which direction do you want to move? (up, down, left, right or none)");
-            switch((input.nextLine()).toUpperCase()){
-                case "DOWN":
-                    playerToken.move(PlayerToken.Move.DOWN);
-                    break;
-                case "UP":
-                    playerToken.move(PlayerToken.Move.UP);
-                    break;
-                case "RIGHT":
-                    playerToken.move(PlayerToken.Move.RIGHT);
-                    break;
-                case "LEFT":
-                    playerToken.move(PlayerToken.Move.LEFT);
-                    break;
-                case "BREAK":
-                    flag = false;
-                    break;
-                default:
-                    playerToken.move(PlayerToken.Move.NONE);
-            }
-        }
+        Game game = new Game();
+        game.start();
     }
 }
